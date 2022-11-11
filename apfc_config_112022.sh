f146c792f590703587ba7a9c648ff64f938cd83f
a1ca1 <66342355+a1ca1@users.noreply.github.com>
2022-11-11 09:17:31 -0500

    Update apfc_config_112022.sh

--- apfc_config_112022.sh
+++ apfc_config_112022.sh
@@ -4,9 +4,9 @@ echo "Starting configuration"
 echo "setting up the brewery"
  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/apfc/.zprofile
+#apple silicon only: echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/apfc/.zprofile
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+#apple silicon only: eval "$(/opt/homebrew/bin/brew shellenv)"
 
 
 #installing software
